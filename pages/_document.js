c9f73ac7702b8555d22493bb7c55b4185dea1879
Daine Mawer <hello@dainemawer.com>
2022-07-22 14:27:19 +0200

    add search console verification

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <meta name="google-site-verification" content="tnIVeeV3aqBwVttZrYQn8iqI8tvBjEtKvSYje9TXQwk" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
